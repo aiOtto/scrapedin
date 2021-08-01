fdb8733c6f6d340ac1b45d9e63e11ddc9342bcec
Arjay Nacion <acnacion87@gmail.com>
2021-08-01 09:31:10 +0800

    * Test fix to get profile section

--- src/profile/seeMoreButtons.js
+++ src/profile/seeMoreButtons.js
@@ -5,13 +5,13 @@ const seeMoreButtons = [
     selector: '.pv-about-section inline-show-more-text__button'
   },{
     id: 'SHOW_MORE_EXPERIENCES',
-    selector: '#experience-section .pv-profile-section__see-more-inline'
+    selector: '#experience-section .profile__see-more-inline'
   },{
     id: 'SEE_MORE_EXPERIENCES',
     selector: '#experience-section .inline-show-more-text__button'
   },{
     id: 'SHOW_MORE_CERTIFICATIONS',
-    selector: '#certifications-section .pv-profile-section__see-more-inline'
+    selector: '#certifications-section .profile__see-more-inline'
   },{
     id: 'SHOW_MORE_SKILLS',
     selector: '.pv-skills-section__additional-skills'
